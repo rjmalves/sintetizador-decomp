2d1e87870e1a2231c81372133b38e90e125c05ee
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-12-01 10:27:58 -0300

    fix async

--- sintetizador/utils/encoding.py
+++ sintetizador/utils/encoding.py
@@ -8,11 +8,11 @@ import subprocess
 TIMEOUT_DEFAULT = 10.0
 
 
-def converte_codificacao(path: str, script: str):
-    _, out = run_terminal_retry([f"file -i {path}"])
+async def converte_codificacao(path: str, script: str):
+    _, out = await run_terminal_retry([f"file -i {path}"])
     cod = out[0].split("charset=")[1].strip()
     if "unknown" in cod:
         cod = "ISO-8859-1"
     if all([cod != "utf-8", cod != "us-ascii", cod != "binary"]):
         cod = cod.upper()
-        c, _ = run_terminal_retry([f"{script}" + f" {path} {cod}"])
+        c, _ = await run_terminal_retry([f"{script}" + f" {path} {cod}"])
