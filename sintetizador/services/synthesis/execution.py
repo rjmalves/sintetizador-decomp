cafee0987b50367a72880b6f8f44f8075c286fab
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-12-02 13:20:12 -0300

    fix dZinf

--- sintetizador/services/synthesis/execution.py
+++ sintetizador/services/synthesis/execution.py
@@ -120,12 +120,12 @@ class ExecutionSynthetizer:
             - df_processed["tempo"].to_numpy()[:-1]
         )
         df_processed["dZinf"] = df_processed["zinf"]
-        df_processed.at[0, "zinf"] = np.nan
         df_processed.loc[1:, "dZinf"] = (
             df_processed["zinf"].to_numpy()[1:]
             - df_processed["zinf"].to_numpy()[:-1]
         )
         df_processed.loc[1:, "dZinf"] /= df_processed["zinf"].to_numpy()[:-1]
+        df_processed.at[0, "zinf"] = np.nan
         return df_processed
 
     def _resolve_tempo(self) -> pd.DataFrame:
