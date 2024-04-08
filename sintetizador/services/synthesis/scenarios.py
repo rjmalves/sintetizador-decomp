15a54b46e0988dffc33d24b04c8c772ce7d1a751
marianasnoel <marianasimoesnoel@gmail.com>
2024-04-08 11:37:40 -0300

    substitui metodos no system scenarios

--- sintetizador/services/synthesis/scenarios.py
+++ sintetizador/services/synthesis/scenarios.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, List, Optional
+from typing import Callable, Dict, List
 import pandas as pd  # type: ignore
 
 from sintetizador.services.unitofwork import AbstractUnitOfWork
@@ -12,23 +12,20 @@ class ScenarioSynthetizer:
         "PROBABILIDADES",
     ]
 
-    def __init__(self) -> None:
-        self.__uow: Optional[AbstractUnitOfWork] = None
-        self.__rules: Dict[Variable, Callable] = {
-            Variable.PROBABILIDADES: self._resolve_probabilities,
+    @classmethod
+    def _get_rule(cls, s: ScenarioSynthesis) -> Callable:
+        rules = Dict[Variable, Callable] = {
+            Variable.PROBABILIDADES: cls._resolve_probabilities,
         }
+        return rules[s]
 
-    @property
-    def uow(self) -> AbstractUnitOfWork:
-        if self.__uow is None:
-            raise RuntimeError()
-        return self.__uow
+    @classmethod
+    def _default_args(cls) -> List[str]:
+        return cls.DEFAULT_SCENARIO_SYNTHESIS_ARGS
 
-    def _default_args(self) -> List[str]:
-        return self.__class__.DEFAULT_SCENARIO_SYNTHESIS_ARGS
-
+    @classmethod
     def _process_variable_arguments(
-        self,
+        cls,
         args: List[str],
     ) -> List[ScenarioSynthesis]:
         args_data = [ScenarioSynthesis.factory(c) for c in args]
@@ -41,17 +38,19 @@ class ScenarioSynthetizer:
                 return []
         return valid_args
 
+    @classmethod
     def filter_valid_variables(
-        self, variables: List[ScenarioSynthesis]
+        cls, variables: List[ScenarioSynthesis]
     ) -> List[ScenarioSynthesis]:
         logger = Log.log()
         if logger is not None:
             logger.info(f"Variáveis: {variables}")
         return variables
 
-    def _resolve_probabilities(self) -> pd.DataFrame:
-        with self.uow:
-            vaz = self.uow.files.get_vazoes()
+    @classmethod
+    def _resolve_probabilities(cls, uow: AbstractUnitOfWork) -> pd.DataFrame:
+        with uow:
+            vaz = uow.files.get_vazoes()
 
         df = vaz.probabilidades
         if df is None:
@@ -66,17 +65,17 @@ class ScenarioSynthetizer:
         ].drop_duplicates(ignore_index=True)
         return df_subset
 
-    def synthetize(self, variables: List[str], uow: AbstractUnitOfWork):
-        self.__uow = uow
+    @classmethod
+    def synthetize(cls, variables: List[str], uow: AbstractUnitOfWork):
         if len(variables) == 0:
-            variables = self._default_args()
+            variables = cls._default_args()
         logger = Log.log()
-        synthesis_variables = self._process_variable_arguments(variables)
-        valid_synthesis = self.filter_valid_variables(synthesis_variables)
+        synthesis_variables = cls._process_variable_arguments(variables)
+        valid_synthesis = cls.filter_valid_variables(synthesis_variables)
         for s in valid_synthesis:
             filename = str(s)
             if logger is not None:
                 logger.info(f"Realizando síntese de {filename}")
-            df = self.__rules[s.variable]()
-            with self.uow:
-                self.uow.export.synthetize_df(df, filename)
+            df = cls._get_rule(s.variable)(uow)
+            with uow:
+                uow.export.synthetize_df(df, filename)
