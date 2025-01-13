3cae434a4405467acdf76974feade16577b6e709
Rogerio Alves <rogerioalves.ee@gmail.com>
2025-01-13 08:52:10 -0300

    logging to stdout

--- app/utils/log.py
+++ app/utils/log.py
@@ -1,5 +1,6 @@
 import logging
 import logging.handlers
+import sys
 from typing import Optional
 
 from app.utils.singleton import Singleton
@@ -9,11 +10,11 @@ class Log(metaclass=Singleton):
     LOGGER = None
 
     @classmethod
-    def configure_logging(cls, diretorio: str):
+    def configure_logging(cls):
         root = logging.getLogger("main")
         f = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
         # Logger para STDOUT
-        std_h = logging.StreamHandler()
+        std_h = logging.StreamHandler(stream=sys.stdout)
         std_h.setFormatter(f)
         root.addHandler(std_h)
         root.setLevel(logging.INFO)
